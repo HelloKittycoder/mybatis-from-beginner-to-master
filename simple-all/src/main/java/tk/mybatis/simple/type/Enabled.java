ba250a13961393e1ea52359f54b27604b1767b42
HelloKittycoder <shucheng2015@outlook.com>
2023-01-26 16:50:22 +0800

    6.3.2 使用自定义的类型处理器

--- simple-all/src/main/java/tk/mybatis/simple/type/Enabled.java
+++ simple-all/src/main/java/tk/mybatis/simple/type/Enabled.java
@@ -5,6 +5,16 @@ package tk.mybatis.simple.type;
  * @date 2023/1/26 15:12
  */
 public enum Enabled {
-    disabled, // 禁用
-    enabled; // 启用
+    disabled(0), // 禁用
+    enabled(1); // 启用
+
+    private final int value;
+
+    Enabled(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
